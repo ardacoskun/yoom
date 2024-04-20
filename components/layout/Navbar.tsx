b8c0d32b02ee9cb4365f54eefde159f5ad60f56d
Arda Coşkun <m.ardacoskun@gmail.com>
2024-04-20 16:57:29 +0300

    clerk auth added

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,12 +1,16 @@
-import MobileNav from "./MobileNav";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import Logo from "../common/Logo";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   return (
     <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Logo />
       <div className="flex-between gap-5">
-        {/* CLERT USER MANAGEMENT */}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
         <MobileNav />
       </div>
     </nav>
