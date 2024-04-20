2d5d952020e0b45af6d93327209354b6b4606914
Arda Coşkun <m.ardacoskun@gmail.com>
2024-04-20 15:23:09 +0300

    sidebar added

---
+++ constants/index.ts
@@ -0,0 +1,32 @@
+export const sidebarLinks = [
+  {
+    id: 1,
+    label: "Home",
+    route: "/",
+    imgUrl: "/icons/Home.svg",
+  },
+  {
+    id: 2,
+    label: "Upcoming",
+    route: "/upcoming",
+    imgUrl: "/icons/upcoming.svg",
+  },
+  {
+    id: 3,
+    label: "Previous",
+    route: "/previous",
+    imgUrl: "/icons/previous.svg",
+  },
+  {
+    id: 4,
+    label: "Recordings",
+    route: "/recordings",
+    imgUrl: "/icons/Video.svg",
+  },
+  {
+    id: 5,
+    label: "Personal Room",
+    route: "/personal-room",
+    imgUrl: "/icons/add-personal.svg",
+  },
+];
