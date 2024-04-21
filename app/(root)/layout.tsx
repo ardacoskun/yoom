046a694ebde352266866d0a8d13e2888f4f14a46
Arda Coşkun <m.ardacoskun@gmail.com>
2024-04-21 21:05:09 +0300

    stream initialized,create new meeting functions added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,12 @@
 import { ReactNode } from "react";
+import StreamVideoProvider from "@/providers/StreamClientProvider";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
 };
 
 export default Layout;
